d096f8c00bcee2a70e0aaa9a056845e321465609
Shriya_HW <shriyadubey220@gmail.com>
2024-09-22 14:27:54 +0530

    22 September

--- Movies.sql
+++ Movies.sql
@@ -92,7 +92,45 @@ INSERT INTO reviewer VALUES
 (9005,"Simon Wright"),
 (9006,"Paul Mocks");
 
+INSERT INTO rating VALUES
+(901,9001,8.40,263575),
+(902,9002,7.90,20207),
+(903,9003,8.30,202778),
+(904,9004,8.20,20678),
+(905,9005,9.10,45678);
 
+INSERT INTO actor VALUES
+(101,"James","Starwet","M"),
+(102,"Debborah","Kerr","M"),
+(103,"Peter","Otoole","M"),
+(104,"Nicole","Madman","F"),
+(105,"Harrison","Ford","F"),
+(106,"Stephen","Baldwin","M");
+
+INSERT INTO movie_cast VALUES
+(101,901,"John Scottie Frogon"),
+(102,902,"Miss Giddens"),
+(103,903,"T.E Lawrence"),
+(104,904,"Michael");
+
+INSERT INTO director VALUES
+(201,"Alfred","lean"),
+(202,"Jack","Forman"),
+(203,"David","Scott");
+
+DESC movie_direction;
+SELECT * FROM movie_direction;
+INSERT INTO movie_direction VALUES
+(201,901),
+(202,902),
+(203,903);
+
+
+
+
+UPDATE rating SET rev_stars=null WHERE mov_id=904;
+SELECT * FROM rating;
+USE movie;
 SELECT * FROM movie;
 SELECT mov_title,mov_year FROM movie;
 
@@ -107,3 +145,132 @@ FROM reviewer
 UNION
 SELECT movie.mov_title
 FROM movie;
+
+SELECT r.rev_name FROM reviewer r,rating a
+WHERE
+r.rev_id=a.rev_id
+AND a.rev_stars>=9;
+
+SELECT m.mov_title FROM movie m,rating r
+WHERE m.mov_id=r.mov_id
+AND rev_stars is null;
+
+SELECT mov_title FROM movie WHERE mov_id IN (901,902,903);
+
+SELECT mov_id,mov_title,mov_year FROM movie WHERE mov_title LIKE 'The%'  
+ORDER BY mov_year ASC;
+
+SELECT * FROM actor;
+
+SELECT act_id FROM actor WHERE act_fname='Peter'
+AND act_lname= 'Otoole';
+
+-- SubQueries
+USE movie;
+SELECT * FROM movie_cast;
+SELECT * FROM movie;
+
+SELECT * 
+FROM actor 
+WHERE act_id IN(
+  -- Selecting 'act_id' from 'movie_cast' where 'mov_id' is in the subquery result
+  SELECT act_id 
+  FROM movie_cast 
+  WHERE mov_id IN (
+    -- Selecting 'mov_id' from 'movie' where 'mov_title' is 'Annie Hall'
+    SELECT mov_id 
+    FROM movie 
+    WHERE mov_title='The Deer Hunter'
+  )
+);
+
+-- Alternative solution
+SELECT a.*
+FROM actor a, movie_cast mc, movie m
+WHERE a.act_id = mc.act_id
+AND mc.mov_id = m.mov_id
+AND m.mov_title = 'The Deer Hunter';
+
+USE movie;
+
+SELECT * from movie WHERE mov_rel_country NOT IN ('UK');
+
+SELECT * FROM director;
+SELECT mov_title FROM movie WHERE mov_id =
+(SELECT mov_id FROM movie_direction WHERE dir_id =
+(SELECT dir_id FROM director WHERE dir_fname="Jack" AND dir_lname="Forman"));
+
+SELECT mov_title, MAX(rev_stars)
+FROM movie, rating 
+WHERE movie.mov_id = rating.mov_id 
+  AND rating.rev_stars IS NOT NULL
+-- Grouping the result set by mov_title
+GROUP BY mov_title
+-- Sorting the result set by mov_title
+ORDER BY mov_title;
+
+-- JOINS
+
+SELECT * FROM movie;
+SELECT a.act_fname,a.act_lname,mc.role FROM actor a
+JOIN movie_cast mc ON a.act_id=mc.act_id
+JOIN movie m ON mc.mov_id=m.mov_id AND m.mov_title="Vertigo";
+
+SELECT DISTINCT mov_year
+FROM movie
+INNER JOIN rating 
+ON movie.mov_id = rating.mov_id
+WHERE rev_stars IN (3, 4)
+ORDER BY mov_year;
+
+
+SELECT * FROM movie_cast;
+SELECT * FROM actor;
+SELECT * FROM movie;
+SELECT * FROM movie_direction;
+
+SELECT count(mov_year) AS Count_movie FROM movie WHERE mov_year=1999;
+SELECT * FROM movie WHERE mov_title LIKE "V%";
+
+-- SELECT * from actor WHERE act_id = (SELECT mov_title FROM movie WHERE mov_id=901);
+SELECT * FROM movie WHERE mov_id IN (SELECT * FROM movie_cast WHERE act_id IN (SELECT * FROM actor WHERE act_fname="James")); 
+
+SELECT * 
+FROM movie 
+WHERE mov_id IN(
+  SELECT act_id 
+  FROM movie_cast 
+  WHERE mov_id IN (
+
+    SELECT act_id 
+    FROM actor
+    WHERE act_fname='James'
+  )
+);
+
+
+
+SELECT m.mov_id,m.mov_title FROM movie m
+JOIN movie_cast mc ON m.mov_id=mc.mov_id
+JOIN actor a ON mc.act_id=a.act_id AND act_fname="James";
+
+DELIMITER $$
+CREATE PROCEDURE SetMovieName(IN act_id INT, OUT actName VARCHAR(10))
+BEGIN
+SELECT m.mov_id,m.mov_title FROM movie m
+JOIN movie_cast mc ON m.mov_id=mc.mov_id
+JOIN actor a ON mc.act_id=a.act_id AND act_fname= actName;
+
+END $$
+DELIMITER ;
+
+CALL SetMovieName(901,@actName);
+SELECT @actName;
+DROP PROCEDURE SetMovieName;
+
+
+SELECT * FROM movie;
+SELECT * FROM movie HAVING MAX(mov_time);
+
+
+
--- View_assn.sql
+++ View_assn.sql
@@ -69,6 +69,128 @@ WHERE salary>(SELECT AVG(salary) FROM employee);
 
 SELECT * FROM EmployeeDetails;
 SELECT * FROM department;
+
 CREATE VIEW EmployeeDet AS 
-SELECT id,first_name,last_name,salary,department.dept_name
-FROM employee WHERE salary>(SELECT AVG(salary) FROM employee);
+SELECT e.id,e.first_name,e.last_name,e.salary,d.dept_name
+FROM employee e,department d;
+
+SELECT * FROM EmployeeDet;
+
+SELECT * FROM employee;
+DESC employee;
+
+CREATE TABLE employee1(
+emp_id INT PRIMARY KEY,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+email VARCHAR(50),
+mobile VARCHAR(50),
+hire_date DATE,
+job_id INT,
+salary FLOAT,
+manager_id INT,
+dept_id INT
+);
+
+DROP TABLE employee1;
+
+CREATE TABLE department1(
+dept_id INT,
+dept_name VARCHAR(50),
+manager_id INT,
+location_id INT
+
+);
+SELECT * FROM employee1;
+
+INSERT INTO employee1 VALUES
+(101,"Manish","Kumar","mk@gmail.com",9876543,'2024-02-22',21,25000,99,65),
+(102,"Nitin","Josh","nj@gmail.com",9876523,'2024-03-22',22,24000,100,66),
+(103,"Shetal","Singh","ss@gmail.com",9126543,'2024-02-28',23,23000,101,67);
+
+SELECT * FROM department1;
+INSERT INTO department1 VALUES
+(65,"HR",99,1),
+(66,"IT",100,2);
+
+DROP TABLE department1;
+
+
+DELIMITER $$
+CREATE PROCEDURE get_employee()
+BEGIN
+SELECT e.first_name,e.last_name,e.email,d.dept_name 
+FROM 
+employee1 e
+JOIN 
+department1 d
+ON e.dept_id=d.dept_id;
+
+END $$
+DELIMITER ;
+
+
+CALL get_employee();
+
+DROP PROCEDURE get_employee;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE GetLimitedEmployee(IN limit_count INT)
+BEGIN
+    SELECT 
+        first_name, 
+        last_name, 
+        salary, 
+        hire_date
+    FROM 
+        employee1
+    LIMIT limit_count;
+END$$
+
+DELIMITER ;
+
+CALL GetLimitedEmployee(3);
+
+
+DELIMITER $$
+
+CREATE PROCEDURE CountEmployee(OUT total_count INT)
+BEGIN
+    SELECT 
+        count(*) INTO 
+        total_count
+    FROM 
+        employee1;    
+END$$
+DELIMITER ;
+
+CALL CountEmployee(@employee_count);
+SELECT @employee_count;
+
+
+DELIMITER $$
+
+CREATE FUNCTION GetEmployeeGrade(salary DECIMAL(10, 2)) 
+RETURNS CHAR(1)
+BEGIN
+    DECLARE grade CHAR(1);
+
+    IF salary BETWEEN 0 AND 10000 THEN
+        SET grade = 'D';
+    ELSEIF salary BETWEEN 10001 AND 30000 THEN
+        SET grade = 'C';
+    ELSEIF salary BETWEEN 30001 AND 60000 THEN
+        SET grade = 'B';
+    ELSE
+        SET grade = 'A';
+    END IF;
+
+    RETURN grade;
+END$$
+
+DELIMITER ;
+
+SELECT first_name, salary, GetEmployeeGrade(salary) AS grade
+FROM employee1;
